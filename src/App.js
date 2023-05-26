6c1504e0f0dfd58ea7b6d5e72489240384bf9b8f
Farahalh <120371923+Farahalh@users.noreply.github.com>
2023-05-26 16:28:56 +0200

    Cleaning up code, adding bootstrap, building weather app html with fake data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.css";
+import "./App.css";
+import Weather from "./Weather";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <Weather />
+      <footer>
+        This project was coded by {""}
+        <a href="https://farahalh.com/" target="_blank" rel="noreferrer">
+          Farah Alhaddad
         </a>
-      </header>
+         {""}and is{""}
+         <a href="https://github.com/Farahalh" target="_blank" rel="noreferrer">
+          open-sourced on Github
+        </a>
+      </footer>
     </div>
   );
 }
-
-export default App;
---
+++ src/Weather.js
@@ -0,0 +1,43 @@
+import "./weather.css";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city..."
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="Search" className="btn btn-primary" />
+          </div>
+        </div>
+      </form>
+      <h1>New York</h1>
+      <ul>
+        <li>Friday 4:00</li>
+        <li>Mostly Sunny</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+            alt="Mostly Cloudy"
+          />
+          16°C
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 0%</li>
+            <li>Humidity: 25%</li>
+            <li>Wind: 5km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
