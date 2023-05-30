fc2d1764ce4099111f36691ffad95fae543cc24d
Farahalh <120371923+Farahalh@users.noreply.github.com>
2023-05-30 10:56:09 +0200

    Adding basic CSS and imported axios

--- src/Weather.js
+++ src/Weather.js
@@ -1,3 +1,5 @@
+import React from "react";
+import axios from "axios";
 import "./weather.css";
 
 export default function Weather() {
@@ -10,10 +12,15 @@ export default function Weather() {
               type="search"
               placeholder="Enter a city..."
               className="form-control"
+              autoFocus="on"
             />
           </div>
           <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary" />
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
           </div>
         </div>
       </form>
@@ -22,13 +29,18 @@ export default function Weather() {
         <li>Friday 4:00</li>
         <li>Mostly Sunny</li>
       </ul>
-      <div className="row">
+      <div className="row mt-3
+      ">
         <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-            alt="Mostly Cloudy"
-          />
-          16°C
+          <div className="clearfix">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+              alt="Mostly Cloudy"
+              className="float-left"
+            />
+            <span className="temperature">16</span>
+            <span className="unit">°C</span>
+          </div>
         </div>
         <div className="col-6">
           <ul>
