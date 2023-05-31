0d2a7975f80fa4cd2dcb1fa806f5609c1e586f06
Farahalh <120371923+Farahalh@users.noreply.github.com>
2023-05-31 12:19:27 +0200

    Added API call and debugging code

--- src/Weather.js
+++ src/Weather.js
@@ -1,55 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
-import "./weather.css";
+import "./Weather.css";
 
 export default function Weather() {
-  return (
-    <div className="Weather">
-      <form>
-        <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Enter a city..."
-              className="form-control"
-              autoFocus="on"
-            />
+  const [ready, setReady] = useState(false);
+  const [temperature, setTemperature] = useState(null);
+  
+  function handleResponse(response) {
+    console.log(response.data);
+    setTemperature(response.data.temperature.current);
+    setReady(true);
+  }
+
+  if (ready) {
+    return (
+      <div className="Weather">
+        <form>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city..."
+                className="form-control"
+                autoFocus="on"
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100"
+              />
+            </div>
           </div>
-          <div className="col-3">
-            <input
-              type="submit"
-              value="Search"
-              className="btn btn-primary w-100"
-            />
+        </form>
+        <h1>New York</h1>
+        <ul>
+          <li>Friday 4:00</li>
+          <li>Mostly Sunny</li>
+        </ul>
+        <div
+          className="row mt-3
+      "
+        >
+          <div className="col-6">
+            <div className="clearfix">
+              <img
+                src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+                alt="Mostly Cloudy"
+                className="float-left"
+              />
+              <span className="temperature">{temperature}</span>
+              <span className="unit">°C</span>
+            </div>
           </div>
-        </div>
-      </form>
-      <h1>New York</h1>
-      <ul>
-        <li>Friday 4:00</li>
-        <li>Mostly Sunny</li>
-      </ul>
-      <div className="row mt-3
-      ">
-        <div className="col-6">
-          <div className="clearfix">
-            <img
-              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-              alt="Mostly Cloudy"
-              className="float-left"
-            />
-            <span className="temperature">16</span>
-            <span className="unit">°C</span>
+          <div className="col-6">
+            <ul>
+              <li>Precipitation: 0%</li>
+              <li>Humidity: 25%</li>
+              <li>Wind: 5km/h</li>
+            </ul>
           </div>
-        </div>
-        <div className="col-6">
-          <ul>
-            <li>Precipitation: 0%</li>
-            <li>Humidity: 25%</li>
-            <li>Wind: 5km/h</li>
-          </ul>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "980ta46f70b3b386c063344ca8aof7b9";
+    let city = "New York";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading...";
+  }
 }
