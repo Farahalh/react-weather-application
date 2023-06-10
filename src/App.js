f70aeb84802ae25365d9499ca1b7c21d1be1fbbe
Farahalh <120371923+Farahalh@users.noreply.github.com>
2023-06-10 22:01:43 +0200

    Updating footer layout

--- src/App.js
+++ src/App.js
@@ -5,15 +5,15 @@ import Weather from "./Weather";
 export default function App() {
   return (
     <div className="App">
-      <Weather defaultCity="Stockholm"/>
+      <Weather defaultCity="Stockholm" />
       <footer>
         This project was coded by {""}
         <a href="https://farahalh.com/" target="_blank" rel="noreferrer">
           Farah Alhaddad
         </a>
-         {""} and is {""}
-         <a href="https://github.com/Farahalh" target="_blank" rel="noreferrer">
-          open-sourced on Github
+        {""} and is open-sourced on {""}
+        <a href="https://github.com/Farahalh" target="_blank" rel="noreferrer">
+        Github
         </a>
       </footer>
     </div>
